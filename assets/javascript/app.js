19fc3e0255f1b26613d83f7095c131431b9c0468
Charles Danner <danner.ced1@gmail.com>
2019-08-14 16:28:44 -0400

    <done with JS minus the reset button. Still need to make it pretty tho.>

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,54 +1,94 @@
-var correct = 0;
-var incorrect = 0;
-var unanswered = 0;
-var timeLeft = 20;
-var currentQuestion = 0;
+$(document).ready(function () {
+    var correct = 0;
+    var incorrect = 0;
+    var unanswered = 0;
+    var timeLeft = 20;
+    var currentQuestion = 0;
+    var domIntervalDisplay
+    var questionTimerOut
 
-function timeLeftFunction() {
-    timeLeft--;                 //used to keep track of time and populate the DOM
-    $("#timeLeft").html("Time remaining: " + timeLeft);
-}
-
-function newQuestion(){
-    setInterval(timeLeftFunction, 1000);    //interval set to the timeLeft function which counts down every second per the 1000
-    $(".question" + currentQuestion).show()
-}
-
-
-$("#start-game").on("click", function () {
-    $("#start-game").hide();
-    newQuestion()
-   
-})
-
-function answerSelect (){
-
-    function verdict(){
-         //clear interval
-         $(".question" + currentQuestion).hide();
-         $(".verdict").show();
-         console.log("Chicken")
+    function timeLeftFunction() {
+        timeLeft--;                 //used to keep track of time and populate the DOM
+        $("#timeLeft").html("Time remaining: " + timeLeft);
     }
-    $(".question").on("click", function(event){
-        console.log(event.currentTarget.id);
-        if(event.currentTarget.id === "correct"){
-            correct ++;
-            console.log(correct)
+
+    function verdict() {
+        clearInterval(domIntervalDisplay)
+        clearTimeout(questionTimerOut)
+        $(".question" + currentQuestion).hide();
+        $(".verdict").show();
+        timeLeft = 20;
+
+    }
+
+    function nextQuestion() {
+        if (currentQuestion === 6) {
+            setTimeout(function () {
+                $(".verdict").hide();
+                $("#reset-game").show();
+                $(".results").show();
+                $("#correct-answers").html("You answered " + correct + " answers correctly");
+                $("#incorrect-answers").html("You answered " + incorrect + " answers incorrectly");
+                $("#unanswered-questions").html("You did not answer " + unanswered + " questions in time")
+            }, 6000)
+        }
+        else {
+            setTimeout(function () {
+                $(".verdict").hide();
+                newQuestion();
+            }, 6000)
+        }
+    }
+
+    function newQuestion() {
+        $(".question" + currentQuestion).show()
+        domIntervalDisplay = setInterval(timeLeftFunction, 1000);    //interval set to the timeLeft function which counts down every second per the 1000
+        questionTimerOut = setTimeout(function () {
+            unanswered++;
             verdict();
-            $("#result").html("Correct!")
+            $("#result").html("Too slow!");
             $("#gif").html("<img src='assets/images/question" + currentQuestion + ".gif'>")
             currentQuestion++
-            newQuestion()
+            nextQuestion()
+        }, 21 * 1000)
+    }
+
+
+    $("#start-game").on("click", function () {
+
+        function answerSelect() {
+
+            $(".question" + currentQuestion).show()
+
+            $(".question").on("click", function (event) {
+                if (event.currentTarget.id === "correct") {
+
+                    correct++;
+                    verdict();
+                    $("#result").html("Correct!")
+                    $("#gif").html("<img src='assets/images/question" + currentQuestion + ".gif'>")
+                    currentQuestion++
+                    nextQuestion();
+
+                }
+                if (event.currentTarget.id === "incorrect") {
+
+                    incorrect++;
+                    verdict();
+                    $("#result").html("Nope! Maybe this gif will help you for out next time!");
+                    $("#gif").html("<img src='assets/images/question" + currentQuestion + ".gif'>")
+                    currentQuestion++
+                    nextQuestion();
+
+                }
+            })
+
         }
-        if(event.currentTarget.id === "incorrect"){
-            incorrect++;
-            verdict();
-            $("#result.html").html("Nope! Maybe this gif will help you out next time!");
-            $("#gif").html("<img src='assets/images/question" + currentQuestion + ".gif'>")
-            currentQuestion++
-            newQuestion()
-        }
+        $("#start-game").hide();
+        //interval set to the timeLeft function which counts down every second per the 1000
+        answerSelect();
+        newQuestion();
+
     })
-    
-}
-answerSelect()
+
+})
